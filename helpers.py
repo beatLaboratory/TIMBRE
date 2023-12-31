f75a08f391ba5e9897b25b020dd293773dfa1758
synapticleft <agarwalled@gmail.com>
2023-12-30 22:18:46 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -12,6 +12,50 @@ from keras import utils as np_utils
 from random import sample
 from scipy import signal
 
+def carrier_based(X,Y,inds_test,inds_train,learn_rate=.001,is_categorical=True):
+  """
+  Predicts output using demodulated LFP and linear regression
+  
+  Parameters:
+  - X = Multi-channel data (T samples x N channels, complex-valued)
+  - Y = Category labels (T samples, integer-valued)
+  - inds_test = test indices (Either T x 1 boolean, or U x 1 integers)
+  - inds_train = train indices (Either T x 1 boolean, or U x 1 integers)
+  - learn_rate = how quickly the network learns
+  - is_categorical = whether the output consists of discrete classes 
+ 
+  Returns:
+  - model: trained network
+  - fittedModel: history of loss and accuracy for test and train data
+  """
+  #demodulate the LFP
+  X = X*np.exp(1j*-np.angle(X[:,0][:,np.newaxis]))
+  #stack the real and imaginary components of the data
+  X = np.concatenate((np.real(X), np.imag(X)), axis = 1) 
+  #use one-hot encoding for the class labels
+  if is_categorical:
+      Y = np_utils.to_categorical(Y)
+      my_loss = 'categorical_crossentropy'
+  else:
+      my_loss = 'kde'
+  backend.clear_session()
+  # Early Stopping: stop training model when test loss stops decreasing
+  es = EarlyStopping(monitor = 'val_loss', patience = 1)
+  # Specify the algorithm and step size used by gradient descent
+  adam = optimizers.Adam(learning_rate=learn_rate)
+  num_chans = Y.shape[1]
+  model = models.Sequential()
+  # Layer 1: Takes a complex-valued projection of the input
+  model.add(complexnn.dense.ComplexDense(num_chans, input_shape=(X.shape[1],), use_bias=False, kernel_constraint = constraints.unit_norm()))
+  # Layer 2: Softmax of layer 2
+  model.add(layers.Activation(activations.softmax))
+  model.compile(loss=my_loss, optimizer=adam,metrics = ['accuracy'])
+  # Train the model
+  fittedModel = model.fit(X[inds_train,:], Y[inds_train,:], epochs = 100,
+    verbose = 2, validation_data=(X[inds_test,:], Y[inds_test,:]),
+    shuffle=True, callbacks=[es])
+  return model, fittedModel
+
 def TIMBRE(X,Y,inds_test,inds_train,hidden_nodes=0,learn_rate=.001,is_categorical=True):
   """
   Learns oscillatory patterns that are predictive of class labels
