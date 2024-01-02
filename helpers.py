42087ecc99a769f64e5c5e6f3ce0d8232c9ca652
synapticleft <agarwalled@gmail.com>
2024-01-02 11:19:34 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -12,50 +12,6 @@ from keras import utils as np_utils
 from random import sample
 from scipy import signal
 
-def carrier_based(X,Y,inds_test,inds_train,learn_rate=.001,is_categorical=True):
-  """
-  Predicts output using demodulated LFP and linear regression
-  
-  Parameters:
-  - X = Multi-channel data (T samples x N channels, complex-valued)
-  - Y = Category labels (T samples, integer-valued)
-  - inds_test = test indices (Either T x 1 boolean, or U x 1 integers)
-  - inds_train = train indices (Either T x 1 boolean, or U x 1 integers)
-  - learn_rate = how quickly the network learns
-  - is_categorical = whether the output consists of discrete classes 
- 
-  Returns:
-  - model: trained network
-  - fittedModel: history of loss and accuracy for test and train data
-  """
-  #demodulate the LFP
-  X = X*np.exp(1j*-np.angle(X[:,0][:,np.newaxis]))
-  #stack the real and imaginary components of the data
-  X = np.concatenate((np.real(X), np.imag(X)), axis = 1) 
-  #use one-hot encoding for the class labels
-  if is_categorical:
-      Y = np_utils.to_categorical(Y)
-      my_loss = 'categorical_crossentropy'
-  else:
-      my_loss = 'kde'
-  backend.clear_session()
-  # Early Stopping: stop training model when test loss stops decreasing
-  es = EarlyStopping(monitor = 'val_loss', patience = 1)
-  # Specify the algorithm and step size used by gradient descent
-  adam = optimizers.Adam(learning_rate=learn_rate)
-  num_chans = Y.shape[1]
-  model = models.Sequential()
-  # Layer 1: Takes a complex-valued projection of the input
-  model.add(layers.Dense(num_chans, input_shape=(X.shape[1],)))#, use_bias=False, kernel_constraint = constraints.unit_norm()))
-  # Layer 2: Softmax of layer 2
-  model.add(layers.Activation(activations.softmax))
-  model.compile(loss=my_loss, optimizer=adam,metrics = ['accuracy'])
-  # Train the model
-  fittedModel = model.fit(X[inds_train,:], Y[inds_train,:], epochs = 100,
-    verbose = 2, validation_data=(X[inds_test,:], Y[inds_test,:]),
-    shuffle=True, callbacks=[es])
-  return model, fittedModel
-
 def TIMBRE(X,Y,inds_test,inds_train,hidden_nodes=0,learn_rate=.001,is_categorical=True):
   """
   Learns oscillatory patterns that are predictive of class labels
@@ -72,6 +28,7 @@ def TIMBRE(X,Y,inds_test,inds_train,hidden_nodes=0,learn_rate=.001,is_categorica
   Returns:
   - model: trained network
   - fittedModel: history of loss and accuracy for test and train data
+  - test_acc: accuracy on test data after training
   """
   
   #stack the real and imaginary components of the data
@@ -105,7 +62,63 @@ def TIMBRE(X,Y,inds_test,inds_train,hidden_nodes=0,learn_rate=.001,is_categorica
   fittedModel = model.fit(X[inds_train,:], Y[inds_train,:], epochs = 100,
     verbose = 2, validation_data=(X[inds_test,:], Y[inds_test,:]),
     shuffle=True, callbacks=[es])
-  return model, fittedModel
+  test_acc = fittedModel.history['val_accuracy'][-1]
+  return model, fittedModel, test_acc
+
+def carrier_based(X,Y,inds_test,inds_train,learn_rate=.001,is_categorical=True,subgroups = []):
+  """
+  Predicts output using demodulated LFP and linear regression
+  
+  Parameters:
+  - X = Multi-channel data (T samples x N channels, complex-valued)
+  - Y = Category labels (T samples, integer-valued)
+  - inds_test = test indices (Either T x 1 boolean, or U x 1 integers)
+  - inds_train = train indices (Either T x 1 boolean, or U x 1 integers)
+  - learn_rate = how quickly the network learns
+  - is_categorical = whether the output consists of discrete classes 
+ 
+  Returns:
+  - model: trained network
+  - fittedModel: history of loss and accuracy for test and train data
+  - test_acc: accuracy on test data after training
+  """
+  #demodulate the LFP using the carrier (defined as the first PC)
+  X = X*np.exp(1j*-np.angle(X[:,0][:,np.newaxis]))
+  #stack the real and imaginary components of the data
+  X = np.concatenate((np.real(X), np.imag(X)), axis = 1)
+  #use one-hot encoding for the class labels
+  if is_categorical:
+      if len(subgroups):
+        Yc = np_utils.to_categorical(Y*np.max(subgroups+1)+subgroups)
+      else:
+        Yc = np_utils.to_categorical(Y)
+      my_loss = 'categorical_crossentropy'
+  else:
+      my_loss = 'kde'
+
+  backend.clear_session()
+  # Early Stopping: stop training model when test loss stops decreasing
+  es = EarlyStopping(monitor = 'val_loss', patience = 1)
+  # Specify the algorithm and step size used by gradient descent
+  adam = optimizers.Adam(learning_rate=learn_rate)
+  num_chans = Yc.shape[1]
+  model = models.Sequential()
+  # Layer 1: Takes a complex-valued projection of the input
+  model.add(layers.Dense(num_chans, input_shape=(X.shape[1],), use_bias=True, kernel_constraint = constraints.unit_norm()))
+  # Layer 2: Softmax of layer 1
+  model.add(layers.Activation(activations.softmax))
+  #model.add(layers.Dense(Y.shape[1],activation='softmax'))
+  model.compile(loss=my_loss, optimizer=adam,metrics = ['accuracy'])
+  # Train the model
+  fittedModel = model.fit(X[inds_train], Yc[inds_train], epochs = 100,
+    verbose = 2, validation_data=(X[inds_test], Yc[inds_test]),
+    shuffle=True, callbacks=[es])
+  if len(subgroups):
+    print(model.predict(X[inds_test]))
+    test_acc = np.mean(np.floor(np.argmax(model.predict(X[inds_test]),axis=1)/np.max(subgroups+1))==Y[inds_test])
+  else:
+    test_acc = fittedModel.history['val_accuracy'][-1]
+  return model, fittedModel, test_acc
 
 def layer_output(X,m,layer_num):
   """
@@ -240,6 +253,25 @@ def filter_data(data, cutoff, fs, filt_type='high', order=5, use_hilbert = False
         data = signal.hilbert(data,axis=0)
         
     return data
+
+def group_by_pos(pos,num_bins):
+    """
+    Subdivides track into bins for training linear classifier on demodulated LFP
+    
+    Parameters
+    ----------
+    pos : a vector that contains the position of the animal along the track
+    num_bins : a scalar int that indicates how many bins to divide the track into
+
+    Returns
+    -------
+    pos : a vector of binned positions
+    """
+    pos = pos - np.min(pos)
+    pos = pos / (np.max(pos)+10**-8)
+    pos = np.int32(np.floor(pos*num_bins))
+    return pos
+
 
 def accumarray(subs, vals, size=None, fill_value=0):
     """
